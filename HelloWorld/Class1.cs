9afba3baf3350ed86bce1def8e38010b92a5c2a8
Ederson Cardoso <>
2020-05-19 15:37:37 -0400

    Add project files.

---
+++ HelloWorld/Class1.cs
@@ -0,0 +1,21 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Class1
+    {
+        [CommandMethod("HelloWorld")]
+        public void HelloWorld()
+        {
+            Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
+            ed.WriteMessage("Hello World");
+        }
+    }
+}
